4063bc147a23b9ba172a453e5b867f142da7bfcf
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2023-02-07 14:48:52 +0700

    tan

---
+++ src/main/java/com/project/QuanLiCLB/QuanLiClbApplication.java
@@ -0,0 +1,13 @@
+package com.project.QuanLiCLB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLiClbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLiClbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/QuanLiCLB/QuanLiClbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.QuanLiCLB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLiClbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
